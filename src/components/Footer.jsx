1c35c5368181d7722a1fa2af6bebe4c2889c104e
Abhinav <abhinav@AVs-Mac.local>
2023-07-04 23:36:19 +0530

    [4 july 2023] creating components, JSX

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import { Logo } from "./Logo";
+
+export const Footer = () => {
+  return (
+    <div>
+      <p>All rights reserved</p>
+      <Logo />
+    </div>
+  );
+};
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,13 @@
+function SearchBar() {
+  return (
+    <div>
+        <div>
+            <label>SearchBar: </label>
+            <input type="text" placeholder="search here" />
+        </div>
+        <button>search</button>
+    </div>
+  );
+}
+
+export default SearchBar;
